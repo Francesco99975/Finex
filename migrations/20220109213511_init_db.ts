1704efde84817b2d2cd83817d8e7c21265c74359
Francesco Michele Barranca <francescomich99@gmail.com>
2023-01-17 10:11:25 -0500

    schema created

--- migrations/20220109213511_init_db.ts
+++ migrations/20220109213511_init_db.ts
@@ -1,17 +1,33 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable('posts', (table) => {
-    table.specificType('id', 'CHAR(16)').primary();
-    table.string('slug', 60).notNullable().unique();
-    table.string('title', 80).notNullable();
-    table.text('content');
-    table.timestamp('publishedAt');
-    table.timestamp('createdAt').notNullable().defaultTo(knex.fn.now());
-    table.timestamp('updatedAt').notNullable();
+  await knex.schema.createTable('etfs', (table) => {
+    table.specificType('symbol', 'CHAR(16)').primary();
+    table.string('name', 100).notNullable().unique();
+    table.double('price', 2).notNullable();
+    table.double('price_change', 2).notNullable();
+    table.double('prev_close', 2).notNullable();
+    table.string('percentage_change', 30).notNullable();
+    table.double('open_price', 2).notNullable();
+    table.double('bid', 2).notNullable();
+    table.integer('bid_size', 25).notNullable();
+    table.double('ask', 2).notNullable();
+    table.integer('ask_size', 25).notNullable();
+    table.string('days_range', 30).notNullable();
+    table.string('weeks_range', 30).notNullable();
+    table.integer('volume', 25).notNullable();
+    table.integer('avg_volume', 25).notNullable();
+    table.string('net_assets', 30).notNullable();
+    table.double('nav', 2).notNullable();
+    table.string('pe_ratio', 30);
+    table.string('dividend_yield', 30).notNullable();
+    table.string('ytd_return', 30).notNullable();
+    table.double('beta', 2).notNullable();
+    table.string('expense_ratio', 30).notNullable();
+    table.timestamp('inception_date').notNullable();
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('posts');
+  await knex.schema.dropTable('etfs');
 }
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 import express from 'express';
 import morgan from 'morgan';
-import { nanoid } from 'nanoid';
 
 const db = new PrismaClient({ log: ['error', 'info', 'query', 'warn'] });
-const genId = () => nanoid(16);
-
-const seedDatabase = async () => {
-  if ((await db.post.count()) === 0) {
-    await db.post.createMany({
-      data: [
-        {
-          id: genId(),
-          slug: 'ultimate-node-stack',
-          title: 'Ultimate Node Stack 2022',
-          publishedAt: new Date(),
-        },
-        {
-          id: genId(),
-          slug: 'draft-post',
-          title: 'Draft Post',
-        },
-      ],
-    });
-  }
-};
-seedDatabase();
 
 const app = express();
 app.use(morgan('dev'));
 
 app.get('/', async (req, res) => {
-  const posts = await db.post.findMany();
+  const posts = await db.etf.findMany();
   res.json(posts);
 });
 
